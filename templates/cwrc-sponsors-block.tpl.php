fb0a57b0e894e80034a1ce15f6bbbd1e304e8c20
Nia Kathoni <nikathone@gmail.com>
2018-05-11 16:14:11 +0000

    adding clearfix class to sponsors container.

--- templates/cwrc-sponsors-block.tpl.php
+++ templates/cwrc-sponsors-block.tpl.php
@@ -5,7 +5,7 @@
  * Displays a sponsors block.
  */
 ?>
-<ul class="sponsors">
+<ul class="sponsors clearfix">
   <li class="sponsor-1"><img src="<?php print file_create_url(drupal_get_path('module', 'cwrc_theme_compat') . '/img/INN_CFI_ENB_2C_RGB.png'); ?>" alt="Canada Foundation for Innovation"></li>
   <li class="sponsor-2"><img src="<?php print file_create_url(drupal_get_path('module', 'cwrc_theme_compat') . '/img/sshrc.png'); ?>" alt="Social Sciences and Humanities Research Council of Canada"></li>
   <li class="sponsor-3"><img src="<?php print file_create_url(drupal_get_path('module', 'cwrc_theme_compat') . '/img/compute-canada.png'); ?>" alt="Compute Canada"></li>
